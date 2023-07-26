3865575e5606da61d1ddb017d47b7e64fe0ded30
Василий Морарь <vasili-morari@MacBook-Air-Vasilij.local>
2023-07-26 06:51:09 +0300

    fix/verstcka

--- src/components/CardChatAi/CardChatAi.js
+++ src/components/CardChatAi/CardChatAi.js
@@ -1,27 +1,34 @@
 import "./CardChatAi.css"
 import React from "react";
-import avatar from "../../images/avatar.svg";
-import imgRandom from "../../images/img-random.png";
+import avatarColor from "../../images/avatarColor.svg";
+import imgRandom from "../../images/img-random.svg";
 
 function CardChatAi() {
    return (
       <>
          <div className="chatAi-card">
-            <div>
-               <img src={avatar} alt="логопит" />
-               <div>
-                  <p>Chatty AI</p>
-                  <p>Я хочу помочь тебе с выступлением — запиши аудио, и отправь его мне</p>
+            <div className="chatAi-card__container">
+               <div className="chatAi-card__box">
+                  <div className="chatAi-card__box-img" >
+                     <img src={avatarColor} alt="логопит" />
+                  </div>
+
+                  <div>
+                     <p className="chatAi-card__box-text" >Chatty AI</p>
+                     <p className="chatAi-card__box-text">Я хочу помочь тебе с выступлением — запиши аудио, и отправь его мне</p>
+                  </div>
+               </div>
+
+               <div className="chatAi-card__box">
+                  <data className="chatAi-card__box-data">14.08.23</data>
+                  <img className="chatAi-card__box-photo" src={imgRandom} alt="изображеие от ChatAi" />
+                  <div>
+                     <p className="chatAi-card__box-info">Презентация <br /> Спринта 12</p>
+                     <p className="chatAi-card__box-subinfo">UI design kit</p>
+                  </div>
                </div>
             </div>
-            <div>
-               <data>14.08.23</data>
-               <img src={imgRandom} alt="изображеие от ChatAi" />
-               <div>
-                  <p>Презентация Спринта 12</p>
-                  <p>UI design kit</p>
-               </div>
-            </div>
+
          </div>
       </>
    )
