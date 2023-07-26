09a27a57e1bdebf1457532475c31f0c8bdcf635d
Edward Zhrebets <zhrebec_edvard@mail.ru>
2023-07-26 19:58:00 +0600

    test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,65 +1,65 @@
 import './App.css';
-import { useState, useRef, useEffect } from 'react';
+// import { useState, useRef, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import * as openAiApi from '../../utils/OpenAIApi'
+// import * as openAiApi from '../../utils/OpenAIApi'
 import Saitbar from '../Saitbar/Saitbar';
 import MainScreen from '../MainScreen/MainScreen'
 import ChatAiScreen from '../ChatAiScreen/ChatAiScreen';
 
 
 function App() {
-  const [state, setState] = useState('Initial');
-  const [mediaRecorder, setMediaRecorder] = useState(null);
-  const chunksRef = useRef([]);
-  const [audioURL, setAudioURL] = useState(null)
-  const [transcription, setTranscription] = useState('');
-  const [aiResponse, setAiResponse] = useState('');
+  // const [state, setState] = useState('Initial');
+  // const [mediaRecorder, setMediaRecorder] = useState(null);
+  // const chunksRef = useRef([]);
+  // const [audioURL, setAudioURL] = useState(null)
+  // const [transcription, setTranscription] = useState('');
+  // const [aiResponse, setAiResponse] = useState('');
 
-  useEffect(() => {
-    const getUserMedia = async () => {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      const recorder = new MediaRecorder(stream);
+  // useEffect(() => {
+  //   const getUserMedia = async () => {
+  //     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  //     const recorder = new MediaRecorder(stream);
 
-      recorder.ondataavailable = (e) => {
-        chunksRef.current.push(e.data);
-      }
+  //     recorder.ondataavailable = (e) => {
+  //       chunksRef.current.push(e.data);
+  //     }
 
-      recorder.onstop = async () => {
-        const audioBlob = new Blob(chunksRef.current, { 'type': 'audio/wav' })
-        chunksRef.current = [];
-        setAudioURL(window.URL.createObjectURL(audioBlob))
+  //     recorder.onstop = async () => {
+  //       const audioBlob = new Blob(chunksRef.current, { 'type': 'audio/wav' })
+  //       chunksRef.current = [];
+  //       setAudioURL(window.URL.createObjectURL(audioBlob))
 
-        const audioFile = new File([audioBlob], 'audio.wav', { type: 'audio/wav' });
-        const formData = new FormData();
-        formData.append('file', audioFile);
-        formData.append('model', 'whisper-1');
-        formData.append('language', 'ru')
+  //       const audioFile = new File([audioBlob], 'audio.wav', { type: 'audio/wav' });
+  //       const formData = new FormData();
+  //       formData.append('file', audioFile);
+  //       formData.append('model', 'whisper-1');
+  //       formData.append('language', 'ru')
 
-        try {
-          const transcription = await openAiApi.sendAudio(formData);
-          setTranscription(transcription)
-          const aiResponse = await openAiApi.sendText(transcription);
-          setAiResponse(aiResponse)
-        } catch(error) {
-          console.log(error.message)
-        }
-      }
+  //       try {
+  //         const transcription = await openAiApi.sendAudio(formData);
+  //         setTranscription(transcription)
+  //         const aiResponse = await openAiApi.sendText(transcription);
+  //         setAiResponse(aiResponse)
+  //       } catch(error) {
+  //         console.log(error.message)
+  //       }
+  //     }
+  //     console.log(state, audioURL, transcription, aiResponse)
+  //     setMediaRecorder(recorder);
+  //   }
+  //   getUserMedia()
+  //     .catch(error => console.log('Error accessing microphone:', error));
+  // }, [state, audioURL, transcription, aiResponse])
 
-      setMediaRecorder(recorder);
-    }
-    getUserMedia()
-      .catch(error => console.log('Error accessing microphone:', error));
-  }, [])
+  // function handleStartRecording() {
+  //   setState('Record')
+  //   mediaRecorder.start()
+  // }
 
-  function handleStartRecording() {
-    setState('Record')
-    mediaRecorder.start()
-  }
-
-  function handleStopRecording() {
-    setState('Initial')
-    mediaRecorder.stop();
-  };
+  // function handleStopRecording() {
+  //   setState('Initial')
+  //   mediaRecorder.stop();
+  // };
 
   return (
     // <div className="App">
